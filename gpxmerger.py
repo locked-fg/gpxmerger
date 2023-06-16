5e5766cb21ecfca83fed68423f7ebdba32b942c6
Robert Kampf <Robert.Kampf@gmx.de>
2023-06-16 13:51:56 +0200

    Autoformat with black

--- gpxmerger.py
+++ gpxmerger.py
@@ -11,39 +11,41 @@ ext = ".gpx"
 
 # https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
 logging.basicConfig(level=logging.DEBUG)
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        }
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout'
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "standard": {
+                "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+            }
         },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'gpxmerger.log',
-            'formatter': 'standard'
-        }
-    },
-    'loggers': {
-        '__main__': {  # name my module
-            'level': 'DEBUG',
-            'propagate': True,
-            'handlers': ['file']
-        }
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+            "file": {
+                "level": "DEBUG",
+                "class": "logging.FileHandler",
+                "filename": "gpxmerger.log",
+                "formatter": "standard",
+            },
+        },
+        "loggers": {
+            "__main__": {  # name my module
+                "level": "DEBUG",
+                "propagate": True,
+                "handlers": ["file"],
+            }
+        },
     }
-})
+)
 
 
 def is_gpx(filename):
     logger = logging.getLogger(__name__)
-    logger.debug('Checking {f}'.format(f=filename))
+    logger.debug("Checking {f}".format(f=filename))
     return path.splitext(filename)[1] == ext
 
 
@@ -52,14 +54,14 @@ def load_gpxs(track_files):
     gpxs = []
 
     for track_file in track_files:
-        with open(track_file, 'r') as gpx_file:
+        with open(track_file, "r") as gpx_file:
             gpx_parser = parser.GPXParser(gpx_file)
             gpx_parser.parse()
             gpx = gpx_parser.gpx
             gpxs.append(gpx)
             nsmap.update(gpx.nsmap)
 
-    logger.debug('Loaded a total of {s} files'.format(s=len(gpxs)))
+    logger.debug("Loaded a total of {s} files".format(s=len(gpxs)))
     return gpxs
 
 
@@ -67,7 +69,7 @@ def load_tracks(track_files):
     logger = logging.getLogger(__name__)
     gpxs = load_gpxs(track_files)
     tracks = sum((gpx.tracks for gpx in gpxs), [])
-    logger.debug('Loaded a total of {s} tracks'.format(s=len(tracks)))
+    logger.debug("Loaded a total of {s} tracks".format(s=len(tracks)))
     return tracks
 
 
@@ -75,7 +77,7 @@ def load_segments(track_files):
     logger = logging.getLogger(__name__)
     tracks = load_tracks(track_files)
     segments = sum((track.segments for track in tracks), [])
-    logger.debug('Loaded a total of {s} segments'.format(s=len(segments)))
+    logger.debug("Loaded a total of {s} segments".format(s=len(segments)))
     return segments
 
 
@@ -85,7 +87,7 @@ def load_points(track_files):
     points = sum((segment.points for segment in segments), [])
     points = list(filter(lambda x: x.time is not None, points))
     points = sorted(points, key=lambda p: p.time)
-    logger.debug('Loaded a total of {s} points'.format(s=len(points)))
+    logger.debug("Loaded a total of {s} points".format(s=len(points)))
     return points
 
 
@@ -93,27 +95,29 @@ def get_gpx(data, name=""):
     logger = logging.getLogger(__name__)
     gpx = gpxpy.gpx.GPX()
     gpx.nsmap.update(nsmap)
-    
+
     if isinstance(data[0], gpxpy.gpx.GPXTrack):
-        logger.debug('Generating GPX with {s} tracks'.format(s=len(data)))
+        logger.debug("Generating GPX with {s} tracks".format(s=len(data)))
         gpx.tracks.extend(data)
-        
+
     else:
         # Create first track in our GPX:
         gpx_track = gpxpy.gpx.GPXTrack(name)
         gpx.tracks.append(gpx_track)
-        
+
         if isinstance(data[0], gpxpy.gpx.GPXTrackSegment):
-            logger.debug('Generating GPX with {s} segments'.format(s=len(data)))
+            logger.debug(
+                "Generating GPX with {s} segments".format(s=len(data))
+            )
             gpx_track.segments.extend(data)
-            
+
         elif isinstance(data[0], gpxpy.gpx.GPXTrackPoint):
             # Create first segment in our GPX track:
             gpx_segment = gpxpy.gpx.GPXTrackSegment()
             gpx_track.segments.append(gpx_segment)
-            
-            logger.debug('Generating GPX with {s} points'.format(s=len(data)))
-        
+
+            logger.debug("Generating GPX with {s} points".format(s=len(data)))
+
             # Add points:
             gpx_segment.points.extend(data)
 
@@ -121,11 +125,11 @@ def get_gpx(data, name=""):
 
 
 def save(gpx, target_file):
-    with open(target_file, 'w') as fp:
+    with open(target_file, "w") as fp:
         logger = logging.getLogger(__name__)
-        logger.debug('Saving "{f}"'.format(f=target_file))
+        logger.debug("Saving {f}".format(f=target_file))
         fp.write(gpx.to_xml())
-        logger.debug('Done saving')
+        logger.debug("Done saving")
 
 
 def get_target(files, target=None):
@@ -176,11 +180,17 @@ def merge(files, target=None, segment=False, track=False):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="A simple script to merge multiple GPX files into one large GPX file.")
+    parser = argparse.ArgumentParser(
+        description="A simple script to merge multiple GPX files into one large GPX file."
+    )
     parser.add_argument("input_files", nargs="*", help="input files to merge")
     parser.add_argument("-o", help="output file name, path or directory")
-    parser.add_argument("-s", default=False, action="store_true", help=("merge segments"))
-    parser.add_argument("-t", default=False, action="store_true", help=("merge tracks"))
+    parser.add_argument(
+        "-s", default=False, action="store_true", help=("merge segments")
+    )
+    parser.add_argument(
+        "-t", default=False, action="store_true", help=("merge tracks")
+    )
 
     if len(sys.argv[1:]) == 0:
         parser.print_help()
@@ -190,5 +200,5 @@ def main():
     merge(args.input_files, args.o, args.s, args.t)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
